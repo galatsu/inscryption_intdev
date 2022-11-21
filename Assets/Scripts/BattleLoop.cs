1a55a334366ce222b948c39be0aacb01545c938a
braedon911 <53923231+braedon911@users.noreply.github.com>
2022-11-21 01:42:32 -0500

    tmp add-in + cardobject prefab + battleloop structure

---
+++ Assets/Scripts/BattleLoop.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleLoop : MonoBehaviour
+{
+    public StateMachine stateMachine;
+    Board board;
+
+    private void Awake()
+    {
+        AssembleStatemachine();
+
+    }
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        stateMachine.Execute();
+    }
+    void PlayerFirstTurn()
+    {
+        stateMachine.ChangeState("PlayerEndTurn");
+    }
+    void PlayerDraw()
+    {
+        stateMachine.ChangeState("PlayerTurn");
+    }
+    void PlayerTurn()
+    {
+        stateMachine.ChangeState("PlayerEndTurn");
+    }
+    void PlayerEndTurn()
+    {
+        stateMachine.ChangeState("OpponentTurn");
+    }
+    void OpponentTurn()
+    {
+        stateMachine.ChangeState("OpponentEndTurn");
+    }
+    void OpponentEndTurn()
+    {
+        stateMachine.ChangeState("PlayerDraw");
+    }
+
+    void AssembleStatemachine()
+    {
+        stateMachine = new StateMachine(PlayerFirstTurn, PlayerDraw, PlayerTurn, PlayerEndTurn, OpponentTurn, OpponentEndTurn);
+        stateMachine.ChangeState("PlayerFirstTurn");
+    }
+}
--- Assets/Scripts/CardData.cs
+++ Assets/Scripts/CardData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+[System.Serializable]
 public class CardData
 {
     public string cardName;
@@ -9,6 +10,7 @@ public class CardData
     public int power = 0;
     public int health = 1;
     public int cost = 0;
+    public CardEffect[] effects;
 
     public static CardData UnpackFromAsset(CardAsset asset)
     {
@@ -31,5 +33,49 @@ public class CardData
         asset.cost = data.cost;
 
         return asset;
+    }
+}
+public abstract class CardEffect
+{
+    public string effectName = "";
+    public Sprite icon;
+    public UnityEvent signature;
+
+    void Action()
+    {
+
+    }
+    /*heres the plan: each cardeffect uses a signature and an action.
+    the signature is a game event to listen for, like the start of a turn, or whenever the card is played, or when a card moves into a slot
+    the action is then the thing that the card does in response
+     */
+}
+public class MirrorEffect : CardEffect
+{
+    CardObject target;
+    CardObject personal;
+    MirrorEffect() 
+    { 
+        effectName = "Mirror";
+        signature.AddListener(Action);
+        //signature = when card adjacent changes
+
+    }
+    void Action()
+    {
+        int powerBonus = target.GetPower();
+        personal.SetPower(powerBonus+personal.cardAsset.power);
+    }
+}
+public class CandleEffect : CardEffect
+{
+    CandleEffect()
+    {
+        effectName = "Candle";
+        signature.AddListener(Action);
+    }
+    void Action()
+    {
+        //intercept damage taken and add damage-1 back to health
     }
 }
