4d1d21f994dc1093b76aaca84c2be8a0e795f5a6
William Suwandi <ws2315@nyu.edu>
2022-12-04 02:25:17 -0500

    Working on opponent

--- Assets/Scripts/Opponent.cs
+++ Assets/Scripts/Opponent.cs
@@ -11,13 +11,53 @@ public class Opponent : MonoBehaviour
     [SerializeField]
     Board board;
     [SerializeField]
+    public Hand hand;
+    [SerializeField]
     Deck deck;
     public StateMachine stateMachine;
     public int currentcost = 0;
+    public int numcards = 4;
 
-    public void PlayEnemyCard(int lane)
+    public void DrawFromDeckToHand(int amount = 1)
     {
-        var card = deck.Draw();
-        board.cardSlots[lane, 0].InsertCard(card);
+        for (int i = 0; i < amount; i++)
+        {
+            var card = deck.Draw();
+            if (numcards < 4) { numcards++; }
+            if (card != null) { hand.AddToHand(card); Debug.Log("Added to handtest"); }
+        }
     }
+    void PickFromHand()
+    {
+        var confirmer = hand.TryToPlace();
+    }
+    void PlayCardSelectedToBoard(int lane)
+    {
+        whichcard = Random.Range(0, numcards);
+        cardSelected = hand.cards[whichcard];
+        //if there is a card already in this slot
+        if (board.cardSlots[lane, 2].IsOccupied())
+        {
+            Debug.Log("This lane is already full");
+            hand.RemoveCardConfirmed(false, cardSelected);
+            cardSelected = null;
+        }
+        //if we dont have enough cost to play the card
+        if (currentcost < cardSelected.cardData.cost)
+        {
+            Debug.Log("You don't have enough for this card");
+            hand.RemoveCardConfirmed(false, cardSelected);
+            cardSelected = null;
+        }
+        //if the lane is empty and we have enough to play the card
+        else
+        {
+            hand.RemoveCardConfirmed(true, cardSelected);
+            board.cardSlots[lane, 2].InsertCard(cardSelected);
+            cardSelected = null;
+            Debug.Log("Card played in Lane " + lane);
+        }
+
+    }
+# endr
 }
