1aaeffaeab209bdfe0a523197ae2230e240f149f
William Suwandi <ws2315@nyu.edu>
2022-12-12 04:06:06 -0500

    Tutorial implemented

--- Assets/Scripts/RestartGame.cs
+++ Assets/Scripts/RestartGame.cs
@@ -6,9 +6,22 @@ using UnityEngine.SceneManagement;
 public class RestartGame : MonoBehaviour
 {
     public string sceneToLoad;
+    public GameObject backgroundmusic;
+    string sceneName = "";
+
+    void Awake()
+    {
+        sceneName = SceneManager.GetActiveScene().name;
+    }
 
     public void SceneChange()
     {
+        if (sceneName == "Startup") { DontDestroyOnLoad(backgroundmusic); }
+        else
+        {
+            AudioSource startmusic = GameObject.FindGameObjectWithTag("StartMusic").GetComponent<AudioSource>();
+            startmusic.clip = null;
+        }
         SceneManager.LoadScene(sceneToLoad);
     }
 }
